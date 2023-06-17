921b5fb6b2e6968fb36f4aac523cd11d52f73a99
Matheus Brito <matheusbr032@gmail.com>
2023-06-17 16:18:54 -0300

    fix: remove 'skipTests' from startup

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 POM_APP_VERSION=$(./mvnw -q help:evaluate -Dexpression=project.version -DforceStdout=true)
 
-./mvnw clean package -DskipTests
+./mvnw clean package
 
 docker compose build --build-arg APP_VERSION="$POM_APP_VERSION" --no-cache
 
